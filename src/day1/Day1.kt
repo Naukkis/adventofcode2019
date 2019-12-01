8b55f411aef3b46c594ea1c0e5b642897581ff3e
Ville Naukkarinen <naukkis@gmail.com>
2019-12-01 14:51:02 +0200

    day 1 part 2
    day 1 part 2

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,22 +1,32 @@
 package day1
 
-import java.math.BigDecimal
-import java.math.RoundingMode
-
 class Day1 {
 
     private val input = readFile("input.txt")
 
     init {
-        val fuelRequirement = input.map { value -> value.toBigDecimal() }
-            .map { value ->
-                value.divide(BigDecimal(3), 0, RoundingMode.DOWN)
-                    .subtract(BigDecimal(2))
-            }
-            .fold(BigDecimal.ZERO, BigDecimal::add)
+        val fuelRequirement = input.map { value -> calculateFuelRequirements(value.toInt()) }
+            .sum()
         println(fuelRequirement)
     }
 
-private fun readFile(fileName: String)
-        = this::class.java.getResourceAsStream(fileName).bufferedReader().readLines()
+    private fun calculateFuelRequirements(mass: Int): Int {
+        val initialFuel = divideByThreeScaleDownAndSubtractTwo(mass)
+        return calculateAdditionalFuelRecursive(initialFuel)
+    }
+
+    private fun divideByThreeScaleDownAndSubtractTwo(value: Int): Int {
+        return value / 3 - 2
+    }
+
+    private fun calculateAdditionalFuelRecursive(initialFuel: Int): Int {
+        if (initialFuel < 0) {
+            return 0
+        }
+        return initialFuel + calculateAdditionalFuelRecursive(divideByThreeScaleDownAndSubtractTwo(initialFuel))
+    }
+
+    private fun readFile(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readLines()
+
 }
+
