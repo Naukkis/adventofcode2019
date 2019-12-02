7f4916ab025d2875472e600a307b0daa6aed3516
Ville Naukkarinen <naukkis@gmail.com>
2019-12-02 23:13:56 +0200

    day 2 part 1

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,6 @@
 import day1.Day1
+import day2.Day2
 
 fun main() {
-    Day1()
+    Day2()
 }
--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -1,5 +1,28 @@
 package day2
 
 class Day2 {
+    private val input = readFile("input.txt")
 
+    init {
+        val intCodes = input.split(",")
+            .map(String::toInt)
+            .toMutableList()
+
+        for (pointer in 0 until intCodes.size step 4) {
+            val operation = intCodes[pointer]
+            if (operation == 99) {
+                break
+            }
+            val leftValue = intCodes[intCodes[pointer + 1]]
+            val rightValue = intCodes[intCodes[pointer + 2]]
+            val destination = intCodes[pointer + 3]
+            when (operation) {
+                1 -> intCodes[destination] = leftValue + rightValue
+                2 -> intCodes[destination] = leftValue * rightValue
+            }
+        }
+        println(intCodes[0])
+    }
+
+    private fun readFile(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readLine()
 }
