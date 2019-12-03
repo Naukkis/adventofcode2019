a9f309aed36aeab1cd1ea51c973d89fd84eb8bdc
Ville Naukkarinen <naukkis@gmail.com>
2019-12-03 20:22:45 +0200

    day 2 part 2

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -1,27 +1,44 @@
 package day2
 
 class Day2 {
-    private val input = readFile("input.txt")
+    private val intCodes = readFile("input.txt").split(",")
+        .map(String::toInt)
+        .toMutableList()
 
     init {
-        val intCodes = input.split(",")
-            .map(String::toInt)
-            .toMutableList()
+        findTarget()
+    }
 
-        for (pointer in 0 until intCodes.size step 4) {
-            val operation = intCodes[pointer]
+    private fun calculateInts(inputCodes: MutableList<Int>): Int {
+        for (pointer in 0 until inputCodes.size step 4) {
+            val operation = inputCodes[pointer]
             if (operation == 99) {
                 break
             }
-            val leftValue = intCodes[intCodes[pointer + 1]]
-            val rightValue = intCodes[intCodes[pointer + 2]]
-            val destination = intCodes[pointer + 3]
+            val leftValue = inputCodes[inputCodes[pointer + 1]]
+            val rightValue = inputCodes[inputCodes[pointer + 2]]
+            val destination = inputCodes[pointer + 3]
             when (operation) {
-                1 -> intCodes[destination] = leftValue + rightValue
-                2 -> intCodes[destination] = leftValue * rightValue
+                1 -> inputCodes[destination] = leftValue + rightValue
+                2 -> inputCodes[destination] = leftValue * rightValue
             }
         }
-        println(intCodes[0])
+        return inputCodes[0]
+    }
+
+    private fun findTarget() {
+        val target = 19690720
+        for (noun in 0 until 100) {
+            for (verb in 0 until 100) {
+                val intCodeCopy = intCodes.toMutableList()
+                intCodeCopy[1] = noun
+                intCodeCopy[2] = verb
+                if (target == calculateInts(intCodeCopy)) {
+                    val result = 100 * noun + verb
+                    println(result)
+                }
+            }
+        }
     }
 
     private fun readFile(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readLine()
