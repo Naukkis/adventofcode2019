eaa6847a7e9040ce7c9d9b71b5362256a9a8f361
Ville Naukkarinen <naukkis@gmail.com>
2019-12-08 12:24:32 +0200

    day 6 part 2

--- src/day6/Day6.kt
+++ src/day6/Day6.kt
@@ -4,13 +4,23 @@ class Day6 {
     private val input = readFile("input.txt")
     private val orbiterToPlanet: Map<String, String> =
         input.map { orbit -> orbit.split(")")[1] to orbit.split(")")[0] }.toMap()
+    private var youPath = mutableListOf<String?>()
+    private var sanPath = mutableListOf<String?>()
 
     init {
+        // part 1
         var orbiterPathCount = 0
         for (orbiter in orbiterToPlanet.keys) {
             orbiterPathCount += getOrbitPathCount(orbiter)
         }
         println(orbiterPathCount)
+
+        // part 2
+        getOrbiters(orbiterToPlanet["YOU"], youPath)
+        getOrbiters(orbiterToPlanet["SAN"], sanPath)
+        val firstCommon = sanPath.last { value -> youPath.contains(value) }
+
+        println(sanPath.size - 1 - sanPath.indexOf(firstCommon) + youPath.size - 1 - youPath.indexOf(firstCommon))
     }
 
     private fun getOrbitPathCount(planet: String?): Int {
@@ -22,6 +32,15 @@ class Day6 {
             count += getOrbitPathCount(orbiterToPlanet[planet])
         }
         return count
+    }
+
+    private fun getOrbiters(planet: String?, orbiters: MutableList<String?>) {
+        if (orbiterToPlanet[planet] == null) {
+            return
+        } else {
+            getOrbiters(orbiterToPlanet[planet], orbiters)
+            orbiters.add(planet)
+        }
     }
 
     private fun readFile(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readLines()
